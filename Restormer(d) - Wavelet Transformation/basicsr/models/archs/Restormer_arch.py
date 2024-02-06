796ceacdb68daa3db0afa2e6977daf15bc15fcf8
Himesh Maniyar <himeshmaniyar2017@gmail.com>
2024-02-07 02:21:07 +0530

    Modification

--- Restormer(d) - Wavelet Transformation/basicsr/models/archs/Restormer_arch.py
+++ Restormer(d) - Wavelet Transformation/basicsr/models/archs/Restormer_arch.py
@@ -159,10 +159,11 @@ class Attention(nn.Module):
             cV_list_cuda.append(torch.from_numpy(cV_cuda).cuda())
             cD_list_cuda.append(torch.from_numpy(cD_cuda).cuda())
 
-        cA_list_cuda=torch.from_numpy(cA_list_cuda).cuda()
-        cH_list_cuda=torch.from_numpy(cH_list_cuda).cuda()
-        cV_list_cuda=torch.from_numpy(cV_list_cuda).cuda()
-        cD_list_cuda=torch.from_numpy(cD_list_cuda).cuda()
+        # Convert lists of tensors to single tensors
+        cA_list_cuda = torch.stack(cA_list_cuda)
+        cH_list_cuda = torch.stack(cH_list_cuda)
+        cV_list_cuda = torch.stack(cV_list_cuda)
+        cD_list_cuda = torch.stack(cD_list_cuda)
 
         # Apply quantization and activation functions to the coefficients
         # (Assuming these are PyTorch modules)
